7e96ce9ae000147366fc4c007001e9136792ae2d
Jake Hattwell <stuck@sixpm.dev>
2023-12-19 14:53:41 +1000

    feat: implemented serialization strategy classes

--- antelopy/types/serializables.py
+++ antelopy/types/serializables.py
@@ -11,7 +11,7 @@ class Serializable(Protocol):
         ...
 
 
-class BasicSerializer(Serializable):
+class BasicSerializable(Serializable):
     def __init__(self, value: Any, serialization_strategy: serializers.Serializer):
         self.value = value
         self.strategy = serialization_strategy
@@ -55,20 +55,32 @@ class DictSerializable(Serializable):
 
 
 SERIALIZER_MAP = {
-    "name": serializers.NameSerializer(),
-    "string": serializers.StringSerializer(),
-    "uint8": serializers.NumberSerializer("uint8"),
-    "int8": serializers.NumberSerializer("int8"),
-    "uint16": serializers.NumberSerializer("uint16"),
-    "int16": serializers.NumberSerializer("int16"),
-    "uint32": serializers.NumberSerializer("uint32"),
-    "int32": serializers.NumberSerializer("int32"),
-    "uint64": serializers.NumberSerializer("uint64"),
-    "int64": serializers.NumberSerializer("int64"),
-    "uint128": serializers.NumberSerializer("uint128"),
-    "int128": serializers.NumberSerializer("int128"),
+    "asset": serializers.AssetSerializer(),
+    "bool": serializers.BooleanSerializer(),
+    "bytes": serializers.BytesSerializer(),
+    "checksum160": serializers.ChecksumSerializer(),
+    "checksum256": serializers.ChecksumSerializer(),
+    "checksum512": serializers.ChecksumSerializer(),
     "float32": serializers.NumberSerializer("float32"),
     "float64": serializers.NumberSerializer("float64"),
-    "bytes": serializers.BytesSerializer(),
-    "bool": serializers.BooleanSerializer(),
+    "int8": serializers.NumberSerializer("int8"),
+    "int16": serializers.NumberSerializer("int16"),
+    "int32": serializers.NumberSerializer("int32"),
+    "int64": serializers.NumberSerializer("int64"),
+    "int128": serializers.NumberSerializer("int128"),
+    "name": serializers.NameSerializer(),
+    "public_key": serializers.PublicKeySerializer(),
+    "signature": serializers.SignatureSerializer(),
+    "string": serializers.StringSerializer(),
+    "symbol": serializers.SymbolSerializer(),
+    "symbol_code": serializers.SymbolCodeSerializer(),
+    "time_point": serializers.TimePointSerializer(),
+    "time_point_sec": serializers.TimePointSecSerializer(),
+    "uint8": serializers.NumberSerializer("uint8"),
+    "uint16": serializers.NumberSerializer("uint16"),
+    "uint32": serializers.NumberSerializer("uint32"),
+    "uint64": serializers.NumberSerializer("uint64"),
+    "uint128": serializers.NumberSerializer("uint128"),
+    "varint32": serializers.VarintSerializer(),
+    "varuint32": serializers.VaruintSerializer(),
 }
--- antelopy/types/serializers.py
+++ antelopy/types/serializers.py
@@ -1,22 +1,66 @@
+import binascii
 import struct
+from datetime import datetime
 from typing import Any, Protocol, Tuple, Union
 
-from antelopy.serializers import names, varints
+from antelopy.serializers import assets, keys, names, time_points, varints
 from antelopy.types.types import DEFAULT_TYPES
 
-def split_and_pack_128(n:int):
+
+def split_and_pack_128(n: int):
     if n < 0:
         n = (1 << 128) + n
     buf = b""
-    buf += struct.pack("Q",n&(2**64-1))
-    buf += struct.pack("Q",n >> 64)
+    buf += struct.pack("Q", n & (2**64 - 1))
+    buf += struct.pack("Q", n >> 64)
     return buf
+
 
 class Serializer(Protocol):
     """Base Serializer Protocol"""
 
-    def serialize(self, v: Any) -> Any:
+    def serialize(self, v: Any) -> bytes:
         ...
+
+    def deserialize(self, v: Any) -> bytes:
+        ...
+
+
+class AssetSerializer(Serializer):
+    def serialize(self, v: str) -> bytes:
+        return assets.serialize_asset(v)
+
+    def deserialize(self, v: Any) -> bytes:
+        ...
+
+
+class BooleanSerializer(Serializer):
+    def serialize(self, v: bool) -> bytes:
+        return b"\x01" if v else b"\x00"
+
+    def deserialize(self, v: Any) -> Any:
+        ...
+
+
+class BytesSerializer(Serializer):
+    def serialize(self, v: bytes) -> bytes:
+        return VaruintSerializer().serialize(len(v)) + v
+
+    def deserialize(self, v: Any) -> Any:
+        ...
+
+
+class ChecksumSerializer(Serializer):
+    def serialize(self, v: Union[str, bytes]) -> bytes:
+        if isinstance(v, str):
+            return bytes.fromhex(v)
+        try:
+            v = binascii.unhexlify(v)
+        except binascii.Error:
+            ...
+        if len(v) in [20, 32, 64]:
+            return v
+        raise ValueError("serializing checksums expects str or bytes format")
 
     def deserialize(self, v: Any) -> Any:
         ...
@@ -31,35 +75,45 @@ class NameSerializer(Serializer):
 
 
 class NumberSerializer(Serializer):
-    def __init__(self, type: str):
-        self.type = type
+    def __init__(self, number_type: str):
+        self.type = number_type
 
-    def serialize(self, n: Union[int, float, str]) -> bytes:
-        if isinstance(n,str):
+    def serialize(self, v: Union[int, float, str]) -> bytes:
+        if isinstance(v, str):
             if "int" in self.type:
-                n = int(n)
+                v = int(v)
             elif "float" in self.type:
-                n = float(n)
+                v = float(v)
             else:
-                raise ValueError(f"Value {n} could not be converted to an integer or float")
+                raise ValueError(
+                    f"Value {v} could not be converted to an integer or float"
+                )
         if self.type.endswith("128"):
-            if isinstance(n,float):
+            if isinstance(v, float):
                 # TODO: See if I can implement
-                raise ValueError("Python doesn't do float128s")
-            else:
-                return split_and_pack_128(n)
-        return struct.pack(DEFAULT_TYPES[self.type], n)
+                raise ValueError("Python doesn't handle float128")
+            return split_and_pack_128(v)
+        return struct.pack(DEFAULT_TYPES[self.type], v)
 
     def deserialize(self, v: bytes) -> str:
         ...
 
 
-class BooleanSerializer(Serializer):
-    def serialize(self, v: bool) -> bytes:
-        return b"\x01" if v else b"\x00"
+class PublicKeySerializer(Serializer):
+    def serialize(self, v: str) -> bytes:
+        return keys.serialize_public_key(v)
 
     def deserialize(self, v: Any) -> Any:
         ...
+
+
+class SignatureSerializer(Serializer):
+    def serialize(self, v: str) -> bytes:
+        return keys.serialize_signature(v)
+
+    def deserialize(self, v: Any) -> Any:
+        ...
+
 
 class StringSerializer(Serializer):
     def serialize(self, v: str) -> bytes:
@@ -68,9 +122,35 @@ class StringSerializer(Serializer):
     def deserialize(self, v: Any) -> Any:
         ...
 
-class BytesSerializer(Serializer):
-    def serialize(self, v: bytes) -> bytes:
-        return VaruintSerializer().serialize(len(v)) + v
+
+class SymbolCodeSerializer(Serializer):
+    def serialize(self, v: str) -> bytes:
+        return assets.serialize_symbol_code(v)
+
+    def deserialize(self, v: Any) -> Any:
+        ...
+
+
+class SymbolSerializer(Serializer):
+    def serialize(self, v: str) -> bytes:
+        precision, symbol_name = v.split(",")
+        return assets.serialize_symbol(int(precision), symbol_name)
+
+    def deserialize(self, v: Any) -> Any:
+        ...
+
+
+class TimePointSerializer(Serializer):
+    def serialize(self, v: Union[int, float, datetime]) -> bytes:
+        return time_points.serialize_time_point(v)
+
+    def deserialize(self, v: Any) -> Any:
+        ...
+
+
+class TimePointSecSerializer(Serializer):
+    def serialize(self, v: Union[int, float, datetime]) -> bytes:
+        return time_points.serialize_time_point_sec(v)
 
     def deserialize(self, v: Any) -> Any:
         ...
